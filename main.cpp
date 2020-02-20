24b9988b04cc3a9b7773f7afc9aa6c25302c3ab8
kietsolo99 <kiet.vo054@my.tccd.edu>
2020-02-20 03:01:11 +0000

    result task 4

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include "accounts.h"
+#include <algorithm> //to use the sort command
 using namespace std;
 
 void printArray(int[], int);
 void sortArrDesc(int[], int);
 int main() 
 {
-  //Bubble sort Desc method per task 2 of Unit 6 Assignment
+  //using build-in sort Asc to sort an array
   cout << "\nStart: " << cpuTime() << endl;
-  sortArrDesc(accountBalances, maxAccounts);
+  sort(accountBalances, accountBalances+maxAccounts);
   //calll the sort funtion
   printArray(accountBalances, maxAccounts);
   cout << "\nEnd: " << cpuTime() << endl;
   return 0;
 }
+
 
 //Funtion definition to Print Array
 void printArray(int arrayVals[], int size)
@@ -24,23 +26,4 @@ void printArray(int arrayVals[], int size)
     cout << arrayVals[i] << ",";
   }
 
-}
-
-//Bubble Sort Array Desc
-void sortArrDesc(int arrayVals[], int size)
-{
-int temp = 0;
-for (int left = 0; left < size; left++)
-{
-  for(int right = left + 1; right < size; right++)
-  {
-    if(arrayVals[right] > arrayVals[left])
-    {
-      temp = arrayVals[left];
-      arrayVals[left] = arrayVals[right];
-      arrayVals[right] = temp;
-    }
-  }
-}
-  
 }
